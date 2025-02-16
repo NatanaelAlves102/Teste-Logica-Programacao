b71a2f3e5a97bc094a8c3869f79f06f8cf170b5a
Natanael Alves <natanaelpereiraalves@gmail.com>
2025-02-16 13:52:37 -0300

    terceiro
    Terceiro

--- ExercicioLogicaProgramação/Exercicios/Terceiro/src/App.java
+++ ExercicioLogicaProgramação/Exercicios/Terceiro/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+       
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();  // Nova linha após cada linha de asteriscos
+        }
+        
 }
+
+
+    }
